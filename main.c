c5226d582737ec06446a7ccfc24a12162e9ec6d1
Marshall Clyburn <mardev2013@gmail.com>
2015-08-13 15:54:54 -0400

    Compress 'ifs' to one-liners.

--- main.c
+++ main.c
@@ -15,10 +15,8 @@ int main(int argc, char** args)
 
 	// log file is optional; none specified means default
 	char* log_file_name;
-	if(argc >= 3)
-		log_file_name = args[2];
-	else
-		log_file_name = "depth.log";
+	if(argc >= 3) log_file_name = args[2];
+	else log_file_name = "depth.log";
 	printf("Logging to \'%s\'.\n", log_file_name);
 
 	return 0;
