d569fdd244672046f0697d549aa96a197059b176
Marshall Clyburn <mardev2013@gmail.com>
2015-08-13 16:02:02 -0400

    Open and close the log file.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char** args)
 {
@@ -18,6 +19,27 @@ int main(int argc, char** args)
 	if(argc >= 3) log_file_name = args[2];
 	else log_file_name = "depth.log";
 	printf("Logging to \'%s\'.\n", log_file_name);
+
+	// open log file
+	// always append, losing data is not fun
+	FILE* const log_file = fopen(log_file_name, "a");
+	if(log_file == NULL)
+	{
+		printf("Failed to open \'%s\'.", log_file_name);
+		return 1;
+	}
+	else
+		fprintf(log_file, "NEW SESSION @ %lu\n", time(NULL));
+
+	// main loop
+	// simply read from the sensor and log when necessary
+	// add proper response to SIGINT
+//	for(;;)
+//	{
+//	}
+
+	if(fclose(log_file) == EOF)
+		printf("There was a problem properly closing the log file.\n");
 
 	return 0;
 }
